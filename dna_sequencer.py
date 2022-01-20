42619933f2e21c2f3a56bddbc93e0f0270239d66
Ryan Kelley <kelleyr@duvalschools.org>
2022-01-19 22:13:50 -0500

    Update dna_sequencer.py

--- dna_sequencer.py
+++ dna_sequencer.py
@@ -1,15 +1,12 @@
-# DNA Sequencer, Ryan Kelley, 01/19/22, 9:49PM, v0.2
+# DNA Base Pair Generator, Ryan Kelley, 01/19/22, 10:13PM, v0.4
 # This is a simulated DNA base-pair generator.  It will randomly generate a string of bases, then create the matching string. 
 
 import random 
 
-num_genes = int(input("How many random genes do you want to generate?\n"))
+num_genes = int(input("How many random bases do you want to generate?\n"))
 next_gene = ""
 gene_sequence0 = ""
 gene_sequence1 = ""
-
-
-
 
 x = 0
 while x < num_genes: 
@@ -30,8 +27,19 @@ while x < num_genes:
 print(gene_sequence0)
 
 for i in gene_sequence0: 
-    print(i)
-    # if gene_sequence0[i] == "A":
-    # gene_sequence1 = gene_sequence1 + "T"
+    if i == "A":
+        gene_sequence1 = gene_sequence1 + "T"
+    elif i == "T":
+        gene_sequence1 = gene_sequence1 + "A"
+    elif i == "G":
+        gene_sequence1 = gene_sequence1 + "C"
+    else:
+        gene_sequence1 = gene_sequence1 + "G"
 
+print(gene_sequence1)
+
+# TO-DO List 
+# Sequence Search:  Allow the user to type in a string of base pairs, iterate through the strings to see if that pattern exists in the string. 
+# Simulate mutations in the copied string, i.e. random chance of the wrong base added to the string. 
+# Read / write sequences to disk. 
 
