5334ac852f5dfdd4659e5b832b1325e15542ca6d
Ryan Kelley <kelleyr@duvalschools.org>
2022-01-19 22:01:04 -0500

    v0.2

--- dna_sequencer.py
+++ dna_sequencer.py
@@ -1,22 +1,28 @@
-# DNA Sequencer, Ryan Kelley, 01/19/22, 9:49PM, v0.1
+# DNA Sequencer, Ryan Kelley, 01/19/22, 9:49PM, v0.2
 
 import random 
 
-random_gene = int(random.randint(1,4))
 next_gene = ""
 gene_sequence0 = ""
 gene_sequence1 = ""
+num_genes = int(input("How many random genes do you want to generate?\n"))
+x = 0
 
 
 
-if random_gene == 1: 
-    next_gene = "A"
-elif random_gene == 2:
-    next_gene = "G"
-elif random_gene == 3: 
-    next_gene = "T"
-else: 
-    next_gene = "C"
+while x < num_genes: 
+    random_gene = int(random.randint(1,4))
 
-gene_sequence0 += next_gene
+    if random_gene == 1: 
+        next_gene = "A"
+    elif random_gene == 2:
+        next_gene = "G"
+    elif random_gene == 3: 
+        next_gene = "T"
+    else: 
+        next_gene = "C"
 
+    gene_sequence0 = gene_sequence0 + next_gene
+    x += 1
+
+print(gene_sequence0)
